96dbebf49a6572b73582b7815b2397279ff1633b
miszabi <miszabi@gmail.com>
2015-02-08 21:01:10 +0200

    Update server.js
    Remove unused comments

--- server.js
+++ server.js
@@ -6,20 +6,10 @@ var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
-// configuration ===========================================
-    
-// config files
-//var db = require('./config/db');
-
 // set our port
 var port = process.env.PORT || 8050;
-
-// connect to our mongoDB database 
-// (uncomment after you enter in your own credentials in config/db.js)
-// mongoose.connect(db.url); 
-
-// get all data/stuff of the body (POST) parameters
 // parse application/json 
+
 app.use(bodyParser.json()); 
 
 // parse application/vnd.api+json as json
@@ -44,9 +34,6 @@ require('./app/routes')(app); // configure our routes
 // start app ===============================================
 // startup our app at http://localhost:8080
 app.listen(port);               
-
-// shoutout to the user                     
-console.log('Magic happens on port ' + port);
 
 // expose app           
 exports = module.exports = app;     
